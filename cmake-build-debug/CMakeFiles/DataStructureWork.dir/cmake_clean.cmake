2217358fd19bd211fc41c75729752c7194721403
CharlesZhang <1098795322@qq.com>
2017-06-08 14:54:07 +0800

    Add main

---
+++ cmake-build-debug/CMakeFiles/DataStructureWork.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructureWork.dir/main.cpp.obj"
+  "DataStructureWork.pdb"
+  "DataStructureWork.exe"
+  "DataStructureWork.exe.manifest"
+  "libDataStructureWork.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructureWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
