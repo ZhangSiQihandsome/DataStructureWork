656aa7ae2df9b972b1b60a6538a17bddd7fc3e2d
CharlesZhang <1098795322@qq.com>
2017-06-08 17:13:16 +0800

    Complete Graph file and test Success

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <time.h>
 #include "SparseGraph.h"
+#include "DenseGraph.h"
 
 using namespace std;
 
@@ -12,13 +13,13 @@ int main() {
     srand(time(NULL));
 
     // Sparse Graph
+    cout << "Sparse Graph" << endl;
     SparseGraph g1(N, false);
     for (int i = 0; i < M; ++i) {
         int a = rand() % N; // 随机生成两个数
         int b = rand() % N;
         g1.addEdge(a, b); // 添加一条边
     }
-
     for (int j = 0; j < N; ++j) {
         cout << j << " : ";
         SparseGraph::adjIterator adj(g1, j);
@@ -27,6 +28,23 @@ int main() {
         }
         cout << endl;
     }
+    cout << "----------------------" << endl;
 
+    // Dense Graph
+    cout << "Dense Graph" << endl;
+    DenseGraph g2(N, false);
+    for (int i = 0; i < M; ++i) {
+        int a = rand() % N; // 随机生成两个数
+        int b = rand() % N;
+        g2.addEdge(a, b); // 添加一条边
+    }
+    for (int j = 0; j < N; ++j) {
+        cout << j << " : ";
+        DenseGraph::adjIterator adj(g2, j);
+        for (int w = adj.begin(); !adj.end(); w = adj.next()) {
+            cout << w << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
